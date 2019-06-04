5786e772d415e1ecad3f0756f4ee2924e043e909
eriktx11 <erik@llerenapiano.com>
2019-06-03 21:06:00 -0500

    Feat: Https

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.all('*', (req, res, next)=>{
+  if(req.secure){ return next(); }
+  else { res.redirect(307, 'https://'+req.hostname+':'+app.get('secPort')+req.url); }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
